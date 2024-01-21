7d0eb48f4275a5d282f5c8e1c98abb74d330f3e3
Mark Ghaby <markghaby@gmail.com>
2024-01-21 01:30:42 -0500

    fix

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,8 +6,21 @@
 
 <script>
 import { defineComponent } from "vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default defineComponent({
   name: "IndexPage",
+
+  setup() {
+    onAuthStateChanged(getAuth(), (user) => {
+      if (user) {
+        // User is signed in
+        console.log("User is signed in:", user);
+      } else {
+        // User is signed out
+        console.log("User is signed out");
+      }
+    });
+  },
 });
 </script>
