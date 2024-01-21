a8084f71a39b2ce3debf4658fd3ddc74a591d1fc
Ahmad Nabelsi <ahmad@wearecws.com>
2024-01-21 05:17:40 -0500

    index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,39 @@
 <template>
-  <q-page class="flex flex-center">
-    <p>Hello</p>
+  <q-page padding>
+    <div
+      class="text-h3 text-center q-my-md"
+      style="font-family: 'Playfair', sans-serif"
+    >
+      Introducing... Who's on Break?
+    </div>
+    <div
+      class="text-subtitle1 text-center q-my-md"
+      style="font-family: 'Playfair', sans-serif"
+    >
+      Don't spend your break alone! Upload your schedule and find out who's
+      available in just a few clicks
+    </div>
+    <div class="q-pa-md row justify-center">
+      <q-img
+        src="/src/assets/whos_on_break_logo.png"
+        class="rounded-borders"
+        style="height: 300px; width: 500px; object-fit: cover"
+      ></q-img>
+    </div>
+    <div elevated class="row justify-center q-my-md">
+      <q-btn
+        label="Sign Up"
+        elevated
+        class="bg-teal-400 text-color-white q-mr-md"
+        @click="$router.push('/signup')"
+      />
+      <q-btn
+        label="Log In"
+        outline
+        color="teal-400"
+        @click="$router.push('/login')"
+      />
+    </div>
   </q-page>
 </template>
 
@@ -14,13 +47,17 @@ export default defineComponent({
   setup() {
     onAuthStateChanged(getAuth(), (user) => {
       if (user) {
-        // User is signed in
         console.log("User is signed in:", user);
       } else {
-        // User is signed out
         console.log("User is signed out");
       }
     });
   },
 });
 </script>
+
+<style>
+.rounded-borders {
+  border-radius: 10px;
+}
+</style>
