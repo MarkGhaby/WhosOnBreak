118a85a7573a1a82ffefba3485c8e8d69fec5aee
Mark Ghaby <markghaby@gmail.com>
2024-01-20 19:33:17 -0500

    repo init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="bg-teal-400">
+      <q-toolbar>
+        <q-toolbar-title @click="$router.push('/')">
+          Who's On Break?
+        </q-toolbar-title>
+
+        <div @click="$router.push('/login')">
+          <q-icon name="person"></q-icon>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {},
+
+  setup() {
+    const leftDrawerOpen = ref();
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
